bedad191ae17ec5b70853d2ef0bc23066cb5cfb4
lenhat41555 <70925729+lenhat41555@users.noreply.github.com>
2020-09-15 08:11:49 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GTLNmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+ int[] array; 
+ do {
+     Console.WriteLine("nhập kích thước:");
+     size = Int32.Parse(Console.ReadLine());
+     if (size > 20)
+         Console.WriteLine("khích thước không vượt quá 20");
+ } while (size > 20);
+ array = new int[size];
+int i = 0;
+while (i < array.Length) {
+    Console.WriteLine("nhập phần tử" + (i + 1) + " : ");
+    array[i] = Int32.Parse(Console.ReadLine());
+    i++;
+}
+Console.WriteLine("danh sách tài sản: ");
+ for (int j = 0; j < array.Length; j++) {
+     Console.WriteLine(array[j] + "\t");
+ }
+
+ int max = array[0];
+ int index = 1;
+ for (int j = 0; j < array.Length; j++) {
+     if (array[j] > max) {
+         max = array[j];
+         index = j + 1;
+     }
+ }
+ Console.WriteLine("giá trị lớn nhất là  " + max + " ,ở vị trí " + index);
+
+        }
+    }
+}
